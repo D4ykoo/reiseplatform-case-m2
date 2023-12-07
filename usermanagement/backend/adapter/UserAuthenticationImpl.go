c72a5bd26e5e5ae3db5ee1ea2be968651dc3f79b
Dario Köllner <dario.koellner@gmail.com>
2023-12-07 11:48:22 +0100

    Feature: JWT Auth

--- usermanagement/backend/adapter/UserAuthenticationImpl.go
+++ usermanagement/backend/adapter/UserAuthenticationImpl.go
@@ -1,9 +1,47 @@
 package adapter
 
-func createJWT() {
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
 
+func loadJWTEnv(key string) {
+	// TODO: Load env file and get value
 }
 
-func validateJWT() {
+func createJWT(username string) *string {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS512, jwt.MapClaims{
+		"username": username,
+		"nbf":      time.Date(2015, 10, 10, 12, 0, 0, 0, time.UTC).Unix(),
+	})
 
+	tokenString, err := token.SignedString("your-fav-secret")
+	if err != nil {
+		return nil
+	}
+
+	return &tokenString
+}
+
+func validateJWT(tokenString string) (bool, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		// Don't forget to validate the alg is what you expect:
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+		// hmacSampleSecret is a []byte containing your secret, e.g. []byte("my_secret_key")
+		return []byte("my_secret_key"), nil
+	})
+
+	if err != nil {
+		return false, err
+	}
+
+	if _, ok := token.Claims.(jwt.MapClaims); ok {
+		return true, nil
+	}
+
+	return false, nil
 }
