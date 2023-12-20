25b2330abf39e761ffdde07103074162d9878867
D4ykoo <dario.koellner@gmail.com>
2023-12-20 14:01:01 +0100

    Feature: Init workspace and add jwt validator

---
+++ checkout/backend/jwt-auth/src/lib.rs
@@ -0,0 +1,44 @@
+//! Simple JWT validator that uses HS512 signing algorithm.
+//! 
+//! Claims: username, iat
+//! Examples not due to simplicity not provided. 
+use std::env;
+use dotenvy::dotenv;
+use jsonwebtoken::{Validation, Algorithm, decode, DecodingKey};
+use models::*;
+
+pub mod models;
+
+pub fn validate_jwt(token: &str) -> Result<bool, jsonwebtoken::errors::Error> {
+    dotenv().ok();
+    let jwt_secret = env::var("JWT_SECRET").expect("Secret must be set");
+
+    let mut val = Validation::new(Algorithm::HS512);
+    val.set_required_spec_claims(&["username", "iat"]);
+
+    let decoded = decode::<Claims>(&token, &DecodingKey::from_secret(jwt_secret.as_bytes()), &val);
+
+    match decoded {
+        Ok(_) => return Ok(true),
+        Err(e) => return Err(e),
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::validate_jwt;
+
+    #[test]
+    fn token_valid() {
+        let token = "eyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjY4NjYyMDgwMCwidXNlcm5hbWUiOiJ0ZXN0In0.CbmybuROnf_3ClsxXiYiTbK26Dc0e2zSwMeCZZz4guszI-q8LL6HO42HJTeAjQ0gDFRmL4PikQoP8QzdPC03yw";
+        let result = validate_jwt(token);
+        assert_eq!(result.is_ok(), true);
+    }
+
+    #[test]
+    fn token_invalid(){
+        let token = "pyJhbGciOiJIUzUxMiIsInR5cCI6IkpXVCJ9.eyJpYXQiOjY4NjYyMDgwMCwidXNlcm5hbWUiOiJ0ZXN0In0.CbmybuROnf_3ClsxXiYiTbK26Dc0e2zSwMeCZZz4guszI-q8LL6HO42HJTeAjQ0gDFRmL4PikQoP8QzdPC03yw";
+        let result = validate_jwt(token);
+        assert_eq!(result.is_ok(), false);
+    }
+}
---
+++ checkout/backend/jwt-auth/src/models.rs
@@ -0,0 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Claims {
+    pub username: String,
+    pub iat: usize
+}
