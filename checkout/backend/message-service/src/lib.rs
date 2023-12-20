f3797caced008f20e5bfd7d9f62c28f6f9cbceff
D4ykoo <dario.koellner@gmail.com>
2023-12-20 15:03:50 +0100

    Feature: Add broker usage defined in env

--- checkout/backend/message-service/src/lib.rs
+++ checkout/backend/message-service/src/lib.rs
@@ -1,3 +1,8 @@
+//! Message Service for producing messages and sending them
+//!
+//! Uses async kafka, so it is non blocking.
+//! Uses only one broker defined in the .env file.
+
 use std::env;
 use std::time::Duration;
 use dotenvy::dotenv;
@@ -16,10 +21,12 @@ pub struct MessageProducer {
 }
 
 impl MessageProducer {
-    pub fn init_message_producer(&self, brokers: Vec<String>) {
+    pub fn init_message_producer(&self) {
         dotenv().ok();
+        let broker = vec![env::var("KAFKA_URL").unwrap()];
+
         let producer = &ClientConfig::new()
-            .set("bootstrap.servers", brokers)
+            .set("bootstrap.servers", broker)
             .set("message.timeout.ms", "5000")
             .create()
             .expect("producer creation error");
