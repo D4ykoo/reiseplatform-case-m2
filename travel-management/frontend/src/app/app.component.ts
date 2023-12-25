97657aac7df61c73836b53d3c71454c6fa4f7f49
Michael Gürtner <m_guertner@web.de>
2023-12-25 08:40:16 +0100

    Feature: Add models and table (WIP)

--- travel-management/frontend/src/app/app.component.ts
+++ travel-management/frontend/src/app/app.component.ts
@@ -5,14 +5,29 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { SearchBarComponent } from "./components/search-bar/search-bar.component";
 import { ResultTableComponent } from "./components/result-table/result-table.component";
 import { InfoCardComponent } from "./components/info-card/info-card.component";
+import { FileUploadModule } from 'primeng/fileupload';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [CommonModule, RouterOutlet, NavBarComponent, SearchBarComponent, ResultTableComponent, InfoCardComponent]
+    imports: [CommonModule, RouterOutlet, NavBarComponent, SearchBarComponent, ResultTableComponent, InfoCardComponent, FileUploadModule]
 })
 export class AppComponent {
   title = 'Travel';
+
+  public onFileChange(event:any) {
+    console.log(event);
+    const file = event.files[0];
+    const reader = new FileReader();
+    reader.onload=()=> {
+      const baseString64 = reader.result as string;
+      console.log(baseString64);
+    }
+
+    if(file) {
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- travel-management/frontend/src/app/components/info-card/info-card.component.ts
+++ travel-management/frontend/src/app/components/info-card/info-card.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
+import { DialogModule } from 'primeng/dialog';
 
 @Component({
   selector: 'app-info-card',
   standalone: true,
-  imports: [CardModule,ButtonModule],
+  imports: [CardModule,ButtonModule, DialogModule],
   templateUrl: './info-card.component.html',
   styleUrl: './info-card.component.css'
 })
 export class InfoCardComponent {
+  visible: boolean = false;
 
+  showDialog() {
+      this.visible = !this.visible;
+  }
 }
