a75569eaafeec3318d6040ecd1c7ec8cd754bd42
D4ykoo <dario.koellner@gmail.com>
2023-12-28 17:50:46 +0100

    WIP: Dockerfile for sveltekit with bun

--- checkout/frontend/src/routes/Header.svelte
+++ checkout/frontend/src/routes/Header.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <header>
-	<div class="navbar bg-base-100 top-0 mb-6">
+	<div class="navbar bg-base-100 top-0 mb-6" id="top">
     <div class="navbar-start">
       <div class="dropdown">
         <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
@@ -25,30 +25,30 @@
           </svg>
         </div>
         <ul
-          tabindex="0"
+          tabindex="-1"
           class="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
         >
-          <li><a href="">Login/ Register</a></li>
-          <li><a href="">Usermanagement</a></li>
+          <li><a href="#top">Login/ Register</a></li>
+          <li><a href="#top">Usermanagement</a></li>
         </ul>
       </div>
       <a class="btn btn-ghost text-xl" href="/">travMa</a>
     </div>
     <div class="navbar-center hidden lg:flex">
       <ul class="menu menu-horizontal px-1">
-        <li><a href="/">Login/ Register</a></li>
-        <li><a href="/users">Usermanagement</a></li>
+        <li><a href="#top">Login/ Register</a></li>
+        <li><a href="#top">Usermanagement</a></li>
       </ul>
     </div>
     <div class="navbar-end">
       <div class="mr-4">
-        <a class="btn" href="#">Overview</a>
+        <a class="btn" href="#top">Overview</a>
       </div>
       <div class="mr-4">
-        <a class="btn" href="#">Checkout</a>
+        <a class="btn" href="#top">Checkout</a>
       </div>
       <div>
-        <a class="btn" href="#">Logout</a>
+        <a class="btn" href="#top">Logout</a>
       </div>
     </div>
   </div>
