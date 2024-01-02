798799b290f46767c34dc66164ca7c67b12148cc
Michael Gürtner <m_guertner@web.de>
2024-01-02 16:23:42 +0100

    Feature: Split Frontend and Backend

--- travel-management/frontend/src/assets/env.js
+++ travel-management/frontend/src/assets/env.js
@@ -2,6 +2,6 @@
     window["env"] = window["env"] || {};
   
     // Environment variables
-    window["env"]["API_URL_FR"] = "http://localhost:8083/api/v1/";
+    window["env"]["API_URL_FR"] = "http://localhost:8086/api/v1/";
     window["env"]["DEBUG_FR"] = true;
   })(this);
--- travel-management/frontend/src/environments/environment.ts
+++ travel-management/frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    HotelAPI: window["env" as any]["API_URL_FR" as any] || "http://localhost:8083/api/v1/",
+    HotelAPI: window["env" as any]["API_URL_FR" as any] || "http://localhost:8086/api/v1/",
     debug: window["env" as any]["DEBUG_FR" as any] || false
 };
