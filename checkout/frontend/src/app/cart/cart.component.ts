7a9d7564727161a7b42dbcb1aa9bccb051ae9154
D4ykoo <dario.koellner@gmail.com>
2024-01-02 15:16:37 +0100

    Feature: Add http API endpoint calls

--- checkout/frontend/src/app/cart/cart.component.ts
+++ checkout/frontend/src/app/cart/cart.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { environment } from '../../environment/environment';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { CartService } from '../../services/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -11,22 +12,49 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  constructor(
+    private cartService: CartService,
+  ) {}
+
   hotelmanagementUrl = environment.hotelmanagement_url;
+
+  cartId = 1;
 
   cart = [
     { name: 'Hotel 1', price: 100 },
     { name: 'Hotel 2', price: 200 },
   ];
 
-  constructor() {};
+
+  ngOnInit() {
+    this.getCart();
+  }
+
+  public getCart() {
+    this.cartService.getCart(this.cartId).subscribe((res: any) => {
+      for (const item of res) {
+        this.cart.push(item);
+      }
+    });
+  }
+
+  public removeFromCart(id: number): void {
+    this.cartService.removeFromCart(id).subscribe((res: any) => {
+      console.log(res);
+      this.cart.splice(id, 1);
+    });
+  }
+
+  public addToCart(id: any) {
+    this.cartService.addToCart(id);
+  }
+
 
   public getTotal(): number {
     return this.cart.reduce((sum, item) => sum + item.price, 0);
   }
 
-  public removeFromCart(index: number): void {
-    this.cart.splice(index, 1);
-  }
+
 
   public clearCart(): void {
     this.cart = [];
--- checkout/frontend/src/environment/environment.ts
+++ checkout/frontend/src/environment/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
     production: false,
+    api: 'http://localhost:8080',
     usermanagement_url: 'http://localhost:8081',
     checkout_url: 'http://localhost:8083',
     hotelmanagement_url: 'http://localhost:8085',
